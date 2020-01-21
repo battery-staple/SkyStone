6c2487dfd63a6175b6b51b37279ac0a40f99bad9
Rohen Giralt <rohen_giralt@caryacademy.org>
2020-01-21 12:21:01 -0500

    Added "with" infix function, as well as documentation for the enum Direction.
    Signed-off-by: Rohen Giralt <rohengiralt@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Direction.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Direction.kt
@@ -1,8 +1,25 @@
 package org.firstinspires.ftc.teamcode.helpers
+import kotlin.math.abs
 
+/**
+ * Enum class that maps various linear directions
+ * to an integer multiplier that can be used for
+ * setting the direction of an arbitrary power.
+ */
 enum class Direction(val multiplier: Int) {
     FORWARDS(1),
     BACKWARDS(-1),
     STOP(0);
 }
 
+/**
+ * Simple infix function that returns a number with
+ * the appropriate sign as based on the Direction
+ * passed to it.
+ *
+ * Currently only works with Float, but can be expanded
+ * to other types relatively easily.
+ */
+infix fun Float.with(direction: Direction): Float {
+    return abs(this) * direction.multiplier
+}
